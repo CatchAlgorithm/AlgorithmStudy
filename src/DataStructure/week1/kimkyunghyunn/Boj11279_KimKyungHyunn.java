65552db3be842e078b0f8338b91c8b2969dcce7f
KimKyungHyunn <kdkin00@gmail.com>
2022-07-04 10:48:33 +0900

    Boj11279-성공

---
+++ src/DataStructure/week1/kimkyunghyunn/Boj11279_KimKyungHyunn.java
@@ -0,0 +1,28 @@
+package DataStructure.week1.kimkyunghyunn;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+class Boj11279_KimKyungHyunn {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+        for (int i = 0; i < N; i++) {
+            int n = Integer.parseInt(br.readLine());
+            if (n == 0) {
+                if (queue.isEmpty()) {
+                    System.out.println(0);
+                } else {
+                    System.out.println(queue.poll());
+                }
+            } else {
+                queue.offer(n);
+            }
+        }
+    }
+}
