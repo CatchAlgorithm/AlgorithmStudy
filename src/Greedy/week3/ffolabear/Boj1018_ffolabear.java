fd17ab5ace84ee187005ee73a96df5bad72b725d
ffolabear <ffolabear@gmail.com>
2022-07-17 10:40:59 +0900

    백준 1018 - 체스판 다시 칠하기 백준 2810 - 컵홀더 백준 10814 - 나이순 정렬 성공

---
+++ src/Greedy/week3/ffolabear/Boj1018_ffolabear.java
@@ -0,0 +1,76 @@
+package Greedy.week3.ffolabear;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Boj1018_ffolabear {
+
+    static boolean[][] board;
+    static int N, M;
+    static int min = 64;
+
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        board = new boolean[N][M];
+
+        for (int i = 0; i < N; i++) {
+            String input = br.readLine();
+            for (int j = 0; j < M; j++) {
+                if (input.charAt(j) == 'W') {
+                    board[i][j] = true;
+                } else {
+                    board[i][j] = false;
+                }
+
+            }
+        }
+
+        //경우의 수는 (N - 7) * (M - 7) * 2
+
+        for (int i = 0; i < N - 7; i++) {
+            for (int j = 0; j < M - 7; j++) {
+                isValid(i, j);
+            }
+        }
+
+        bw.append(String.valueOf(min));
+        bw.flush();
+        bw.close();
+
+    }
+
+
+    private static void isValid(int i, int j) {
+
+        int end_x = i + 8;
+        int end_y = j + 8;
+        int count = 0;
+
+        boolean init = board[i][j];
+
+        for (int x = i; x < end_x; x++) {
+            for (int y = j; y < end_y; y++) {
+                if (board[x][y] != init) {
+                    count++;
+                }
+
+                //교대로 색이 바뀌어야 하므로 반대로 바꿔줌
+                init = (!init);
+            }
+            init = (!init);
+        }
+
+        count = Math.min(count, 64 - count);
+        min = Math.min(min, count);
+    }
+
+}
